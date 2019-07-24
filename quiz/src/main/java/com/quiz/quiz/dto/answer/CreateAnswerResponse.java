bbc9c031eb3b1ff036eef484096fe11d628ee223
keri_daniel <daniel.keri@moon42.com>
2019-07-24 17:31:21 +0200

    img

--- quiz/src/main/java/com/quiz/quiz/dto/answer/CreateAnswerResponse.java
+++ quiz/src/main/java/com/quiz/quiz/dto/answer/CreateAnswerResponse.java
@@ -3,6 +3,8 @@ package com.quiz.quiz.dto.answer;
 import lombok.Data;
 import lombok.experimental.Accessors;
 
+import java.util.Base64;
+
 @Data
 @Accessors(chain=true)
 public class CreateAnswerResponse {

c72a78e0624dd91aa9585317c20a51d588b728aa
keri_daniel <daniel.keri@moon42.com>
2019-07-25 17:56:08 +0200

    Update, refactors

--- quiz/src/main/java/com/quiz/quiz/repository/accounts/AdminAccountRepository.java
+++ quiz/src/main/java/com/quiz/quiz/repository/accounts/AdminAccountRepository.java
@@ -3,10 +3,14 @@ package com.quiz.quiz.repository.accounts;
 import com.quiz.quiz.entity.Account;
 import com.quiz.quiz.entity.AdminAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface AdminAccountRepository extends JpaRepository<AdminAccount, UUID> {
     Optional<AdminAccount> findByEmail(String email);
+
+    @Query("SELECT a FROM com.quiz.quiz.entity.AdminAccount a WHERE a.id = ?#{principal.id}")
+    Optional<AdminAccount> getAdminAccountData();
 }

1bffa391fabab1de0f0e5bcbfe79fb10e77c43aa
keri_daniel <daniel.keri@moon42.com>
2019-08-06 12:22:15 +0200

    .

--- quiz/src/main/java/com/quiz/quiz/repository/QuestionRepository.java
+++ quiz/src/main/java/com/quiz/quiz/repository/QuestionRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface  QuestionRepository extends JpaRepository<Question, UUID> {
+public interface QuestionRepository extends JpaRepository<Question, UUID> {
 
     Optional<Question> findByTheme(String theme);
 
---
+++ quiz/src/main/java/com/quiz/quiz/services/LoginService.java
@@ -0,0 +1,26 @@
+package com.quiz.quiz.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    public String getLoggedIn(String sessionId) {
+
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String username;
+        if (principal instanceof UserDetails) {
+            username = ((UserDetails)principal).getUsername();
+        } else {
+            username = principal.toString();
+        }
+
+        return username;
+    }
+}
+
+
